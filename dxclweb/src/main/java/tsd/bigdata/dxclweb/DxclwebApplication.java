990de18c8aa3e36c0fc560a5ac7aee1fc8203080
ZTAngel <309819237@qq.com>
2018-05-19 14:19:33 +0800

    first git test.

---
+++ dxclweb/src/main/java/tsd/bigdata/dxclweb/DxclwebApplication.java
@@ -0,0 +1,12 @@
+package tsd.bigdata.dxclweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DxclwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DxclwebApplication.class, args);
+    }
+}
---
+++ sparktrain/src/main/java/tsd/bigdata/sparktrain/SparktrainApplication.java
@@ -0,0 +1,12 @@
+package tsd.bigdata.sparktrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparktrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SparktrainApplication.class, args);
+    }
+}
